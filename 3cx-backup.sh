ddc3a23d4284e637f283ce4c30c816624b9526ee
mstofflett <michaeldst@gmail.com>
2024-01-03 21:01:50 -0500

    Update 3cx-backup.sh
    Fix Capital

--- 3cx-backup.sh
+++ 3cx-backup.sh
@@ -5,17 +5,17 @@ fdisk -l
 
 echo "\nEnter device path (usually /dev/sdb2 or /dev/sdc2)"
 read usb_drive
-echo "\nDownload prereq packages for exFAT support.."
+echo "\nDownload packages for exFAT support.."
 wget http://ftp.us.debian.org/debian/pool/main/f/fuse/fuse_2.9.9-1+deb10u1_amd64.deb
 wget http://ftp.us.debian.org/debian/pool/main/f/fuse-exfat/exfat-fuse_1.3.0-1_amd64.deb
 wget http://ftp.us.debian.org/debian/pool/main/e/exfat-utils/exfat-utils_1.3.0-1_amd64.deb
-echo "\nInstall prereq packages for exFAT support.."
+echo "\nInstall packages for exFAT support.."
 dpkg -i fuse_2.9.9-1+deb10u1_amd64.deb
 dpkg -i exfat-fuse_1.3.0-1_amd64.deb
 dpkg -i exfat-utils_1.3.0-1_amd64.deb
 echo "\nCreate USB ThumbDrive with exFAT support.."
 mkfs.exfat $usb_drive -n "3CXBackup"
-echo "\nPrepare Directory and set permissions"
+echo "\nPrepare Directory and Set Permissions"
 rm -rf /media/usbdrive
 mkdir /media/usbdrive
 chown -R phonesystem /media/usbdrive
