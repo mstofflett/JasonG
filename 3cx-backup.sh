b489b8396d97abe416bb4b2f9bb47c36322aeb87
mstofflett <michaeldst@gmail.com>
2024-01-03 20:50:02 -0500

    Update 3cx-backup.sh
    First Trial

--- 3cx-backup.sh
+++ 3cx-backup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-echo "Today is " `date`
-echo -e "\nList Drives"
+echo "Today is" `date`
+echo "\nList Drives"
 fdisk -l
 
-echo -e "\nenter device path (usually /dev/sdb2 or /dev/sdc2)"
+echo "\nEnter device path (usually /dev/sdb2 or /dev/sdc2)"
 read usb_drive
-
-echo -e "\n you path has the following files and folders: "
-ls $usb_drive
+echo "\nDownload prereq packages for exFAT support.."
+wget http://ftp.us.debian.org/debian/pool/main/f/fuse/fuse_2.9.9-1+deb10u1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/f/fuse-exfat/exfat-fuse_1.3.0-1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/e/exfat-utils/exfat-utils_1.3.0-1_amd64.deb
+echo "\nInstall prereq packages for exFAT support.."
+dpkg -i fuse_2.9.9-1+deb10u1_amd64.deb
+dpkg -i exfat-fuse_1.3.0-1_amd64.deb
+dpkg -i exfat-utils_1.3.0-1_amd64.deb
+echo "\nCreate USB ThumbDrive with exFAT support.."
+mkfs.exfat $usb_drive -n "3CXBackup"
+echo "\nPrepare Directory and set permissions"
+rm -rf /media/usbdrive
+mkdir /media/usbdrive
+chown -r phonesystem /media/usbdrive
+echo "\nMount USB Drive to /media/usbdrive"
+mount $usb_drive /media/usbdrive
+echo "\nOperation Complete"
